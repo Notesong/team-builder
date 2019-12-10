823df18bd22089eed6f2862c60f798d0f074e04c
Notesong <notesong.git@outlook.com>
2019-12-10 00:29:05 -0500

    Add React basic files

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState} from 'react';
+import './App.css';
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState({name: '', email: '', role: ''});
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
